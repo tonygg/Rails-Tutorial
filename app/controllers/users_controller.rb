3142d429d2fb5bfe895c63b36fb072ce27030d11
tonygg <tony_situ@hotmail.com>
2014-01-21 13:03:43 -0500

    Sign in complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+	include SessionsHelper
 
 	def show
 		@user = User.find(params[:id])
@@ -8,15 +9,16 @@ class UsersController < ApplicationController
 		@user = User.new
 	end
 
-	def create
-		@user = User.new(user_params)
-		if @user.save
-			flash[:success] = "Welcome to the Sample App!"
-			redirect_to @user
-		else
-			render 'new'
-		end
-	end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
 
 	private
 
