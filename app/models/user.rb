21c73121b17d61dc0fa0465c41cce95be1ac6846
tonygg <tony_situ@hotmail.com>
2014-01-21 16:17:01 -0500

    Micro posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
+
   before_create :create_remember_token
 	before_save { self.email = email.downcase }
 	validates :name, presence: true, length: { maximum: 50 }
@@ -15,6 +17,11 @@ class User < ActiveRecord::Base
 
   def User.encrypt(token)
     Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    microposts
   end
 
   private
